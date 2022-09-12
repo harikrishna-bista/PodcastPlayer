840e20af93ff2f73c64db7cc960dd11db903d32e
Harikrishna bista <harikrishna.bista@ebpearls.com>
2022-09-12 17:52:42 +0545

    added resources

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PodcastPlayer",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Media.xcassets")]
+        ),
         .testTarget(
             name: "PodcastPlayerTests",
             dependencies: ["PodcastPlayer"]),
